55bc6a1a05350f69205e8af80e6ef90aca3e2951
dmknapp2385 <dmknapp2385@gmail.com>
2021-12-04 15:07:28 -0700

    savecity and error updated

--- js/homescript.js
+++ js/homescript.js
@@ -1,7 +1,4 @@
-var searchCityarray = [
-    // {"day": "current", "temp": , "humidity": "wind", "uv"},
-    // {"day": "tomorrow", etc}
-]
+var searchCityarray = []
 var searchButton = document.querySelector("#search");
 
 // function to get city name when search is hit
@@ -24,11 +21,17 @@ var geoLocate = function(city) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                weatherData(city, data[0].lat, data[0].lon);
+                console.log(data);
+                if (data.length === 0) {
+                    alert("Error: City not Found");
+                }
+                else {
+                    saveCity(city, data[0].lat, data[0].lon);
+                }
             });
         }
         else {
-            alert ("Error: City not Found");
+            alert("Error: Site cannot be reached");
         }
     });
 }
@@ -46,19 +49,30 @@ var weatherData = function(city, lat, lon) {
                 var dateMDY= (date.getMonth() + 1)+ "/" + date.getDate() + "/" + date.getFullYear();
                 // create obj for current day
                 var currentDayObj = {"date": dateMDY, "temp": data.current.temp, "humidity": data.current.humidity, "wind": data.current.wind_speed, "condition": data.current.weather[0].main};
-                
-                // for loop to go over the next five days from daily data in api
-               
-            
-                    // data.weather.main can be clear (thunderstomr, drizzle,rain), snow, clouds, other (hazy conditions)
+
+                // populate today dive with current day stats
+                var todayHeader = document.querySelector(".city").textContent = city.toUpperCase() + " (" + currentDayObj.date + "):";
+
+                // call forecast funciton
+
+                forecast(data.daily);
             });
         }
         else {
             alert ("Error: Cannot get Weather information at this time");
         }
-        saveCity(city, lat, lon);
     });
 }
+
+// forecast function
+var forecast = function(data) {
+    console.log(data);
+}
+
+// for loop to go over the next five days from daily data in api
+               
+            
+                    // data.weather.main can be clear (thunderstomr, drizzle,rain), snow, clouds, other (hazy conditions)
 
 // current day function to populate current day div
 var currentDayStats = function() {
@@ -69,8 +83,10 @@ var currentDayStats = function() {
 
 // store lat and long and city in local storage
 var saveCity = function (city, lat, long) {
-    var cityObj = { "city": city, "latitude": lat, "longitude": long};
-    localStorage.setItem("cities", JSON.stringify(cityObj));
+    var cityObj = {"city": city, "latitude": lat, "longitude": long};
+    searchCityarray.push(cityObj);
+    localStorage.setItem("cities", JSON.stringify(searchCityarray));
+    weatherData(city, lat, long);
 }
 
 // load cities to load most recent five cities
